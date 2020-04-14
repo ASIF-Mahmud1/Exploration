9fc186889893d6cdca4b10637a4ed08c02ad25ef
Asif Mahmud <asif01050105@gmail.com>
2020-04-14 18:23:34 +0600

    separate compoent created: ItemsList,Item. Next task: think how will you pass the state values down the compoent

---
+++ client/solver/Item.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Card, { CardContent, CardActions, CardMedia } from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import seashellImg from './../assets/images/seashell.jpg'
+import Button from 'material-ui/Button'
+import { list, optimize } from '../solver/api-solver'
+import TextField from 'material-ui/TextField'
+import Table, {
+    TableHead,
+    TableRow, TableCell, TableBody
+} from 'material-ui/Table';
+import FurnitureSolver from '../solver/FurnitureSolver.js'
+import { Divider } from 'material-ui'
+const styles = theme => ({
+    card: {
+        //    maxWidth: 600,
+        margin: 'auto',
+        marginTop: theme.spacing.unit * 5
+    },
+    title: {
+        padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
+        color: theme.palette.text.secondary
+    },
+    media: {
+        minHeight: 330
+    }
+})
+
+
+class Item extends Component {
+
+
+
+    render() {
+        const { classes } = this.props
+        return (
+            <TableRow>
+
+                <TableCell><Typography>{this.props.itemDetails.name}Dresser </Typography></TableCell>
+                {
+                    this.props.itemDetails.resources.map((item, index) => {
+                        return (
+                            <TableCell><TextField placeholder="Quantitiy" type="number" value={"None"} onChange={this.handleChange('dresser_wood')} />  </TableCell>
+
+                        )
+                    })
+                }
+            </TableRow>
+        )
+    }
+}
+
+Item.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Item)
---
+++ client/solver/ItemList.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Card, { CardContent, CardActions, CardMedia } from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import seashellImg from './../assets/images/seashell.jpg'
+import Button from 'material-ui/Button'
+import { list, optimize } from '../solver/api-solver'
+import TextField from 'material-ui/TextField'
+import Table, {
+    TableHead,
+    TableRow, TableCell, TableBody
+} from 'material-ui/Table';
+import FurnitureSolver from '../solver/FurnitureSolver.js'
+import { Divider } from 'material-ui'
+import Item from './Item'
+const styles = theme => ({
+    card: {
+        //    maxWidth: 600,
+        margin: 'auto',
+        marginTop: theme.spacing.unit * 5
+    },
+    title: {
+        padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
+        color: theme.palette.text.secondary
+    },
+    media: {
+        minHeight: 330
+    }
+})
+
+
+class ItemList extends Component {
+
+    render() {
+        const { classes } = this.props
+        return (
+
+            <div>
+                <Table>
+                    <TableHead>
+                        <TableCell>Item</TableCell>
+                        <TableCell>Wood</TableCell>
+                        <TableCell>Labor</TableCell>
+                        <TableCell>Profit</TableCell>
+                        <TableCell>Storage</TableCell>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            this.props.items.map((item, index) => {
+                                return (
+                                    <Item itemsDetails={item} />
+                                )
+                            })
+                        }
+                    </TableBody>
+
+                </Table>
+
+            </div>
+        )
+    }
+}
+
+ItemList.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(ItemList)
+/*
+     {
+                    this.props.items.map((item,index)=>{
+                        return(
+                            <Item itemsDetails= {item} />
+                        )
+                    })
+                }
+*/
