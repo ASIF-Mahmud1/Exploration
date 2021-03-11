7290d65a5f51026b43cd18d4013b3feafced49a9
Asif Mahmud <asif01050105@gmail.com>
2021-03-11 12:36:20 +0600

    sample output

---
+++ ouptuts/sampleOutput.js
@@ -0,0 +1,30 @@
+
+//Popular Opinions
+const popularOpinion =
+{
+  brave: { count: 2, eventIdList: [1, 3] },
+  ambitious: { count: 1, eventIdList: [3] }
+}
+
+//Popular tags of  Opinions
+const PopularTagsOfAnOpinion = 
+{
+  brave:
+  {
+    count: 2,
+      eventIdList: [1, 3],
+      tag: 
+      {
+        adventurous: 2, fiction:1,  science: 1
+      }
+  },
+  ambitious:
+  {
+    count: 1,
+      eventIdList: [3],
+      tag: 
+      {
+        science: 1, adventurous: 1
+      }
+  }
+}
